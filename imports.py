23e75c4128daee9fa5ce13a237a0645d97121942
Meera Shersheer <meera04qasemshersheer@gmail.com>
2025-12-28 22:23:03 +0300

    final touches

--- imports.py
+++ imports.py
@@ -1,58 +1,24 @@
 # at the start of the session activate .venv by "source .venv/bin/activate"
 # pip install -r requirements.txt   to reinstall all dependancies
-# Core numerical computing
 import numpy as np
 from numpy import sin, cos, tan, arctan2 as atan2, arccos, arcsin, sqrt, pi
-
-import math
-import re
-
-# Symbolic mathematics (for deriving and verifying equations)
 import sympy as sp
+import math
 from sympy import Symbol, Matrix, simplify, latex, sympify
 from scipy.spatial.transform import Rotation as R
-
-# GUI Framework
 import PyQt6
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
 from PyQt6.QtGui import QAction, QColor, QPalette, QIcon, QKeySequence, QFont,QPainter, QDoubleValidator, QMouseEvent, QPixmap
 import trimesh
-import tempfile
 from PyQt6.QtOpenGLWidgets import QOpenGLWidget
 from OpenGL.GL import *
 from OpenGL.GLU import *
-
 from PyQt6.QtWebEngineWidgets import QWebEngineView
-
-# 3D Visualization
-import matplotlib.pyplot as plot
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.animation as animation
-from matplotlib.figure import Figure
-
-
 from scipy.spatial.transform import Rotation as R
-
-# For better 3D visualization
-import plotly.graph_objects as go
-import plotly.express as px
-
-# For robot visualization (highly recommended!)
-import roboticstoolbox as rtb
-from spatialmath import SE3
-
-# Data handling
-import pandas as pd
-
-# System utilities
-import sys  #for taking the command line args and exit from the program 
+import sys
 import os
-import json # maybe deleted later if not used
 from datetime import datetime
-
-import re
 from random import randint
 from typing import List, Dict, Tuple
 from base_manipulator import *
